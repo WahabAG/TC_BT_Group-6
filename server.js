a8a20cef0340c8f3609af326aa873fd2cab45514
samueldDev <samuelf8908>
2025-05-13 16:29:33 +0100

    re-added wahabAg serverjs file i mistaken deleted during a PR

---
+++ server.js
@@ -0,0 +1,26 @@
+
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+// setting view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve static files from public
+app.use(express.static("public"));
+
+// defining our routes
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "Oluwatobi Adelabu",
+    title: "Backend Developer",
+    bio: "I build APIs and backend systems using Node.js, Express, and MongoDB. I am passionate about creating efficient and scalable applications.",
+  });
+});
+
+// tarting our server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
